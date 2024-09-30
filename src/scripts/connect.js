ec57821d71a752d13f4f78876b8e9b3622ac2d8c
Devlouis_C <DevLouisC@outlook.com>
2024-09-30 18:30:23 -0300

    gerador de notícias
    funcionando em tempo real

--- src/scripts/connect.js
+++ src/scripts/connect.js
@@ -1,13 +1,17 @@
 const newsSection = document.querySelector('.notices')
-
-const GetData = async() =>{
-    const response = await axios.get(
-        'https://newsapi.org/V2/top-headlines?' +
+const inputBtn = document.querySelector('#input-btn')
+const searchInput = document.querySelector('#search-input')
+const comumLink = 
+          'https://newsapi.org/V2/top-headlines?' +
           'country=us&' +
           'apiKey=5a7aa7c4f96447d69a5de31e06f78ac1'
-    )
+
+const GetData = async(otherLink) =>{
+    
+    response = await axios.get(otherLink)
     const data = response.data
     const allNews = data.articles
+    newsSection.innerHTML = ''
     allNews.forEach(news => {
         const notice = document.createElement('article')
         notice.classList.add('news')
@@ -17,6 +21,7 @@ const GetData = async() =>{
 
         const linkToNotice = document.createElement('a')
         linkToNotice.href = news.url
+        linkToNotice.target = '_Blank'
 
         linkToNotice.textContent = news.source.name
 
@@ -35,6 +40,19 @@ const GetData = async() =>{
         }
 
         newsSection.appendChild(notice)}
+
     });
 }
-GetData()
+GetData(comumLink)
+
+inputBtn.addEventListener('click', () =>{
+    const searchContent = searchInput.value
+    link = `https://newsapi.org/v2/top-headlines?q=${searchContent}&apiKey=5a7aa7c4f96447d69a5de31e06f78ac1`
+    GetData(link)
+})
+searchInput.addEventListener('keyup', () =>{
+    const searchContent = searchInput.value
+    if(searchContent === ''){
+        GetData(comumLink)
+    }
+})
